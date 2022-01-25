98d9ade0d097c0261853b460b0544fb1ff2a1b54
weiwang <winoooops@outlook.com>
2022-01-25 10:36:34 +0800

    feature: adding tests and applied to 07

---
+++ Easy/7.merge_two_sorted_lists/index.ts
@@ -0,0 +1,34 @@
+export class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+export function makeListNode(arr: number[]): ListNode | null {
+  if (!arr.length) return null
+
+  if (arr.length === 1) return new ListNode(arr[0], null)
+
+  let result = arr.reduceRight((prev: ListNode | null, curr: number): ListNode | null => {
+    return new ListNode(curr, prev)
+  }, null)
+  return result
+}
+
+export function mergeTwoList(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  // 如果其中任意一个链表结束,直接把当前链表指向另一个链表(链表内顺序永远正确)
+  if (!l1) return l2
+  if (!l2) return l1
+
+  // 链表指向比较小的那个数字,并且那个数字的下一个node也是比较小的那个
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoList(l1.next, l2)
+    return l1
+  } {
+    l2.next = mergeTwoList(l1, l2.next)
+    return l2
+  }
+}
