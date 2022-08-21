367e0ee583415c2d651ab251f32f41e214bab3fe
Wei Wang <winoooops@outlook.com>
2022-08-21 22:36:27 +0800

    update: fix findlca return undefined issue

--- binary-tree/lowest-common-ancestor/index.ts
+++ binary-tree/lowest-common-ancestor/index.ts
@@ -1,15 +1,17 @@
 import { TreeNode } from "../treenode.type"
 
 export function findLCA(node: TreeNode | null, p: number, q: number): TreeNode | null {
-  if(!node) return null 
-  if(node.val === p || node.val === q) return node
+  if (!node) return null
+  if (node.val === p || node.val === q) return node
 
   let left = findLCA(node.left, p, q)
   let right = findLCA(node.right, p, q)
 
-  if(left && right) return node
-  if(left) return left
-  if(right) return right
+  if (left && right) return node
+  if (left) return left
+  if (right) return right
+
+  return null
 }
 
 
