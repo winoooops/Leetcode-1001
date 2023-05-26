8674d50fad79c2658bd88916cf63d05517d717fa
weiwang <winoooops@outlook.com>
2023-05-26 13:01:35 +0800

    reset

--- array/binary-search/index.ts
+++ array/binary-search/index.ts
@@ -1,8 +1,14 @@
 export function BinarySearch(nums: Number[], target: Number) {
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 1256a90 (some changes but mostly reviewing)
   const length = nums.length  
   
   let left = 0 
   let right = length - 1 
+<<<<<<< HEAD
 
   while(left < right) {
     let middle = Math.floor((left + right) / 2)
@@ -22,3 +28,38 @@ export function BinarySearch(nums: Number[], target: Number) {
   left right = length - 1
   return -1
 }
+=======
+=======
+>>>>>>> parent of ab51a25 (refactor structure of java project and add java solution in array)
+  if (!target && target !== 0) return -1;
+=======
+>>>>>>> parent of 1256a90 (some changes but mostly reviewing)
+
+  while(left < right) {
+    let middle = Math.floor((left + right) / 2)
+
+    if(nums[middle] === target ) return middle
+
+    if(nums[middle] < target) {
+      left = middle + 1
+    } else {
+      right = middle 
+    }
+  } 
+
+  return -1 
+
+<<<<<<< HEAD
+  return -1;
+<<<<<<< HEAD
+}
+>>>>>>> parent of ab51a25 (refactor structure of java project and add java solution in array)
+=======
+}
+>>>>>>> parent of ab51a25 (refactor structure of java project and add java solution in array)
+=======
+  let left = 0 
+  left right = length - 1
+  return -1
+}
+>>>>>>> parent of 1256a90 (some changes but mostly reviewing)
--- array/squares-sorted-array/index.spec.ts
+++ array/squares-sorted-array/index.spec.ts
@@ -2,7 +2,25 @@ import { sortSquares } from './index'
 
 describe("Test Squares of sorted array", () => {
   it('should return array of the same size', () => {
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
     const array = [-4, -1, 0, 3, 10]
     expect(sortSquares(array)).toHaveLength(5) 
+=======
+=======
+>>>>>>> parent of ab51a25 (refactor structure of java project and add java solution in array)
+    const array = [-4, -1, 0, 3, 10];
+    const result = sortSquares(array);
+    expect(result).toHaveLength(5); 
+    expect(result).toEqual([0, 1, 9, 16, 100]);
+<<<<<<< HEAD
+>>>>>>> parent of ab51a25 (refactor structure of java project and add java solution in array)
+=======
+>>>>>>> parent of ab51a25 (refactor structure of java project and add java solution in array)
+=======
+    const array = [-4, -1, 0, 3, 10]
+    expect(sortSquares(array)).toHaveLength(5) 
+>>>>>>> parent of 1256a90 (some changes but mostly reviewing)
   })
 })
